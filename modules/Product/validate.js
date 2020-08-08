83da3e31cc857dd14662aebe63d93a6558e24d17
Zyntelligent Tech <zyntelligent@Zyntelligents-MacBook-Air.local>
2020-08-08 17:10:29 +0700

    Fix word Validate

--- modules/Product/validate.js
+++ modules/Product/validate.js
@@ -4,9 +4,11 @@ const validate = (method) => {
   switch (method) {
     case "add": {
       return [
-        check("productname", "กรอกชื่อสินค้า").isLength({ min: 5 }),
-        check("productdescription", "กรอกคำอธิบาย").isLength({ min: 5 }),
-        check("productprice", "กรอกราคา").isInt({ gt: 0 }),
+        check("productname", "กรอกชื่อสินค้าให้ถูกต้อง").isLength({ min: 5 }),
+        check("productdescription", "กรอกคำอธิบายให้ถูกต้อง").isLength({
+          min: 5,
+        }),
+        check("productprice", "กรอกราคาให้ถูกต้อง").isInt({ gt: 0 }),
       ];
     }
   }
